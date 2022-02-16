7df14acb8fc15d1146bfb73d1e6b0b678d126a1e
Daniel Burkart <burkartd03@gmail.com>
2022-02-16 17:17:38 +0100

    změna tlačítek pro reakce

--- public/js/zak.js
+++ public/js/zak.js
@@ -1,1 +1,17 @@
+const { text } = require("express");
+
 const socket = io();
+
+
+
+const tlacitka = document.getElementById('rtlacitka');
+const btnTempo = document.getElementById('btnTempo');
+function reakce(n)
+{
+    alert('reakce' + n);
+}
+
+function test()
+{
+    document.getElementById('rtlacitka').innerHTML = `fff`;
+}
