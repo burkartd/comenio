91b319056d8bb71e75704261289fd223970d089c
Daniel Burkart <burkartd03@gmail.com>
2022-02-16 14:21:02 +0100

    commit

---
+++ server.js
@@ -0,0 +1,22 @@
+//kód serveru, backend
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public'))); //abysme měli přístup k frontendu
+
+//když se připojí klient
+io.on('connection', socket => {
+    console.log('novy pripojeni...');
+});
+
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Servr frci na portu ${PORT} vole`)); 
