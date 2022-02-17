bd57645487b1d9819aaf295d2e7fe68639c1ffd0
Daniel Burkart <burkartd03@gmail.com>
2022-02-17 10:35:04 +0100

    vyvoj tlacitek na odpovedi
    jeste to nefunguje

--- public/js/zak.js
+++ public/js/zak.js
@@ -15,3 +15,40 @@ function test()
 {
     document.getElementById('rtlacitka').innerHTML = `fff`;
 }
+
+function actTlacitka(akce) //aktualizuje div rtlacitka
+{
+    var fce = new Array[3];
+    var text = new Array[3];
+
+    if(akce == 'reset')
+    {
+        fce = [`actTlacitka('tempo')`, `actTlacitka('problem')`, `actTlacitka('reakce')`];
+        text = [`Tempo hodiny`, `Problémy s učivem`, `Reakce na otázku`];
+    }
+    else if(akce = 'tempo')
+    {
+        fce = [`actTlacitka('reset')`, `actTlacitka('reset')`, `actTlacitka('reset')`];
+        text = [`Tempo 1`, `Tempo 2`, `Tempo 3`];
+    }
+    
+    tlacitka.innerHTML = '';
+
+    tlacitka.innerHTML = `
+            <div onclick="${fce[1]}" class="tlacitko">
+                <h4 class="py-10 px-12">
+                    <p>${text[1]}</p>
+                </h4>
+            </div>
+            <div onclick="${fce[2]}" class='tlacitko'>
+                <h4 class="py-10 px-12 hover:shadow">
+                    <p>${text[2]}</p>
+                </h4>
+            </div>
+            <div onclick="${fce[3]}" class="tlacitko">
+                <h4 class="py-10 px-12">
+                    <p>${text[3]}</p>
+                </h4>
+            </div>
+    `;
+}
