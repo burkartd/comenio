fc9f197b240ac9ca2bd526709af03ed41ebc8a6c
Daniel Burkart <burkartd03@gmail.com>
2022-02-25 23:40:14 +0100

    opravení json

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,6 @@
 const check = document.querySelector('#check');
 const menu = document.querySelector('#menu');
 
-const socket = io();
-
-// const btn_join = document.getElementById('button');
-
-// btn_join.onclick = function()
-// {
-
-// }
-
 check.addEventListener('click', () => {
     if(menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
