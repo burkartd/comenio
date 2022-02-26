65a8b46f612817e47b806d640d7ad8b3f9929b1f
Daniel Burkart <burkartd03@gmail.com>
2022-02-26 12:07:15 +0100

    pridani src

--- public/js/ucitel.js
+++ public/js/ucitel.js
@@ -1,6 +1,7 @@
 const socket = io();
 
 var roomNumber = -1; //číslo roomky, získá se v callback funkci
+var roomName = '';
 
 var zaci = []; //kolekce připojených žáků
 
@@ -14,6 +15,7 @@ const divRoomNumber = document.getElementById('roomNumber');
 socket.on('connect', () =>
 {
     socket.emit('hostConnect', data => {
+        roomName = data;
         roomNumber = data.substring(4);
         divRoomNumber.innerHTML = ('#' + roomNumber);
     });
