db1de64dab474b750b4744faf68a91c5d41eb42d
Daniel Burkart <burkartd03@gmail.com>
2022-03-10 10:06:21 +0100

    napojení na backend

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ const frm = document.getElementById("frm");
 const btn = document.getElementById("btnSubmit");
 
 const roomInput = document.getElementById('roominput');
+const nameInput = document.getElementById('nameinput');
 
 //Enter == zmáčknutí tlačítka
 roomInput.addEventListener("keyup", function(event) {
@@ -16,18 +17,30 @@ roomInput.addEventListener("keyup", function(event) {
   //tlačítko potvrzení formu
 btn.addEventListener('click', () => {
     
-    var roomnum = roomInput.value;
+  str = nameInput.value;
+  var regExp = /[a-zA-Z]/g;
 
-    //kontrola jestli místnost existuje
-    socket.emit('checkRoom', roomnum, (data) => {
-        if(data == false)
-        {
-            roomInput.value = '';
-            roomInput.focus();
-        } 
-        else{
-         frm.submit();
-         document.getElementById("theForm").submit();
-        }
+  if(str.length < 1 || !regExp.test(str))
+  {
+    //špatný input jména
+    console.log('spatne jmeno')
+    nameInput.value = '';
+    nameInput.focus();
+    return;
+  }
+    
+  var roomnum = roomInput.value;
+
+  //kontrola jestli místnost existuje
+  socket.emit('checkRoom', roomnum, (data) => {
+      if(data == false)
+      {
+          roomInput.value = '';
+          roomInput.focus();
+          return;
+      } 
+    
+      frm.submit();
+      document.getElementById("theForm").submit();
      });
 });
--- public/js/ucitel.js
+++ public/js/ucitel.js
@@ -6,6 +6,7 @@ var roomName = '';
 var pocetZaku = 0;
 var hotovychZaku = 0;
 var procent = 0.0;
+var pocetUpozorneni = 0;
 
 var zaci = []; //kolekce připojených žáků
 
@@ -22,7 +23,22 @@ seznamZaku.innerHTML = '';
 const seznamUpozorneni = document.getElementById('seznamUpozorneni');
 seznamUpozorneni.innerHTML = '';
 
+const zaku = document.getElementById('pocetzaku');
+zaku.innerHTML = '0';
+
+const vymazatvse = document.getElementById('odstranitvse');
+
+vymazatvse.addEventListener('click', () => {
+    seznamUpozorneni.innerHTML = '';
+    pocetUpozorneni = 0;
+}); //odhlášení z místnosti
+
 var zpravy = ['Nestíhám zápis', 'Nerozumím učivu', 'Zvládám bez problému']; //předvolené zprávy
+
+function pocetZakuUpdate()
+{
+    zaku.innerHTML = pocetZaku;
+}
 
 //při připojení
 socket.on('connect', () =>
@@ -31,19 +47,24 @@ socket.on('connect', () =>
         roomName = data;
         roomNum = data.substring(4);
         divRoomNumber.innerHTML = ('#' + roomNum);
+        console.log(data);
     });
     
     id = socket.id; 
+
+    pocetZakuUpdate();
 });
 
 socket.on('newUser', (user) => {
     prihlaseni(user);
     pocetZaku++;
+    pocetZakuUpdate();
 });
 
 socket.on('userLeft', (id) => {
     odhlaseni(id);
     pocetZaku--;
+    pocetZakuUpdate();
 })
 
 socket.on('upozorneni', (msg, name, druh) => {   
@@ -120,11 +141,10 @@ function zaciUpdate()
         const div = document.createElement('div');
         div.classList.add('zaznam');
         div.innerHTML = `
-        <span class="text-black px-2">${element.userName}</span>
-                        <svg class="w-6 text-comeniodark rounded-md" fill="none" 
-                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7">
-                        </path></svg>`
+        <span class="text-comeniowhiteblue pr-4">${element.userName}</span>
+                    <svg class="w-8 text-comeniowhiteblue bg-comenioblue rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
+                     </path></svg>`
     
         seznamZaku.appendChild(div);
         
@@ -134,29 +154,49 @@ function zaciUpdate()
 function vlastniZprava(msg, jmeno)
 {
     const div = document.createElement('div');
-    div.classList.add('zaznam');
+    div.classList.add('zaznamodpoved');
+    div.classList.add('stin');
     div.innerHTML = `
-    <svg class="w-8 text-sky-800 pl-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"></path></svg>
-                    <span class="text-gray-900 px-2 xl:font-medium">${jmeno}:</span>
-                    <span class="text-gray-900 ">${msg}</span>`
+    <svg class="w-12 text-red-700 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+                    <span class="ml-4">${jmeno}:</span>
+                    <span class="ml-2 font-semibold">${msg}</span>
+                    <svg onclick="smazJeden(this)" class="w-8 ml-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                    <span class="justify-end ml-4 mr-4">${casFormat()}</span>`
 
     div.classList.add('vlastni');
     seznamUpozorneni.appendChild(div);
     seznamUpozorneni.scrollTop = seznamUpozorneni.scrollHeight;
+    pocetUpozorneni++;
 }
 
 function prednastavenaZprava(msg, jmeno)
 {
     const div = document.createElement('div');
-    div.classList.add('zaznam');
+    div.classList.add('zaznamodpoved');
+    div.classList.add('stin');
     div.innerHTML = `
-    <svg class="w-8 text-sky-800 pl-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"></path></svg>
-                    <span class="text-gray-900 px-2 xl:font-medium">${jmeno}:</span>
-                    <span class="text-gray-900 ">${zpravy[msg]}</span>`
+    <svg class="w-12 text-blue-700 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"></path></svg>
+                    <span class="ml-4">${jmeno}:</span>
+                    <span class="ml-2 font-semibold">${zpravy[msg]}</span>
+                    <svg onclick="smazJeden(this)" class="w-8 ml-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                    <span class="justify-end ml-4 mr-4">${casFormat()}</span>`
 
     div.classList.add('prednastavena');
     seznamUpozorneni.appendChild(div);
     seznamUpozorneni.scrollTop = seznamUpozorneni.scrollHeight;
+    pocetUpozorneni++;
+}
+
+function casFormat()
+{
+    var cas = new Date();
+    var h = cas.getHours();
+    var m = cas.getMinutes();
+    return h + ':' + m;
+}
+
+function smazJeden(el)
+{
+    var element = el;
+    element.parentElement.remove();
 }
