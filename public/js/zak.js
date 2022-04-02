bb29a628f560ee30bce52fc66344c983d79efe88
Daniel Burkart <burkartd03@gmail.com>
2022-04-02 16:24:02 +0200

    rozdělení js

--- public/js/zak.js
+++ public/js/zak.js
@@ -1,28 +1,5 @@
-//const { info } = require("autoprefixer");
-
-const socket = io();
-
-var id = socket.id; // id sockety
-var jazyk = 'cz'; //česky
-
-const divRoomNumber = document.getElementById('roomNumber');
-const openclose = document.getElementById('openclose');
-const overlay = document.getElementById('overlay')
-const odhlasit = document.getElementById('odhlasit');
-const anketadiv = document.getElementById('anketapopup');
-const odpovedidiv = document.getElementById('odpovedipopup');
-var jsouOtevrene = false;
-const splneno = document.getElementById('btnsplneno');
-const nesplneno = document.getElementById('btnnesplneno');
-const vlajka = document.getElementById('vlajka');
-const vlajkauk = document.getElementById('ua');
-const vlajkacz = document.getElementById('cz');
 
 divRoomNumber.innerHTML = '#';
-
-const { userName, roomNumber} = Qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  });
 
 odhlasit.addEventListener('click', () => window.history.go(-1)); //odhlášení z místnosti
 
@@ -100,13 +77,6 @@ function openCloseModal(modal)
   overlay.classList.remove('active');
   openclose.classList.remove('otoc');
 }
-
-var roomName = 'room' + roomNumber; //jméno roomky
-
-const Tlacitko1 = document.getElementById('Tlacitko1');
-const Tlacitko2 = document.getElementById('Tlacitko2');
-const Tlacitko3 = document.getElementById('Tlacitko3');
-const vlastni = document.getElementById('VlastniZpravaForm');
 
 document.getElementById('anketazavrit').addEventListener('click', ()=>
 {
