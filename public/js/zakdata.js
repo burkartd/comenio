cccae944081e263f49a819d160bd399234a75749
tom-kroupa <64896728+tom-kroupa@users.noreply.github.com>
2022-08-03 21:34:33 +0200

    uciteldata

--- public/js/zakdata.js
+++ public/js/zakdata.js
@@ -4,77 +4,77 @@
 var zakData = {
     "nadpis": 
         {"cz" :"Upozornit učitele", 
-        "ua":"Попередьте вчителя",
+        "uk":"Попередьте вчителя",
         "de": "Benachrichtigen Sie den Lehrer",
         "pl": "Powiadom nauczyciela",
         "en": "Notify the teacher"},
 
     "sendbtn": 
         {"cz": "Odeslat", 
-        "ua": "Надіслати",
+        "uk": "Надіслати",
         "de": "Senden",
         "pl": "Wysłać",
         "en": "Send"},
 
     "Tlacitko1": 
         {"cz": "Nestíhám zápis", 
-        "ua": "Я не можу зловити рекорд",
+        "uk": "Я не можу зловити рекорд",
         "de": "Ich kann mich nicht registrieren",
         "pl": "nie mogę się zarejestrować",
         "en": "I can't take notes"},
 
     "Tlacitko2": 
         {"cz": "Nerozumím učivu", 
-        "ua": "Я не розумію навчальної програми",
+        "uk": "Я не розумію навчальної програми",
         "de": "Ich verstehe die Lektion nicht",
         "pl": "nie rozumiem lekcji",
         "en": "I don't understand the lesson"},
 
     "Tlacitko3": 
         {"cz": "Potřebuji pomoc", 
-        "ua": "мені потрібна допомога",
+        "uk": "мені потрібна допомога",
         "de": "Ich brauche Hilfe",
         "pl": "potrzebuję pomocy",
         "en": "I need help"},
 
     "hsplneni": 
         {"cz": "Potvrď splnění úkolu", 
-        "ua": "Підтвердіть завдання",
+        "uk": "Підтвердіть завдання",
         "de": "Bestätigen Sie den Abschluss der Aufgabe",
         "pl": "Potwierdź wykonanie zadania",
         "en": "Confirm the completion of the task"},
 
     "btnsplneno": 
         {"cz": "Splněno", 
-        "ua": "Готово",
+        "uk": "Готово",
         "de": "Fertig",
         "pl": "Gotowe",
         "en": "Finished"},
 
     "btnnesplneno": 
         {"cz": "Nesplněno", 
-        "ua": "не зустрічався",
+        "uk": "не зустрічався",
         "de": "Nnerfüllt",
         "pl": "Niespełniony",
         "en": "Not finished"},
 
     "hodpoved": 
         {"cz": "Napiš odpověď", 
-        "ua": "Запишіть відповідь",
+        "uk": "Запишіть відповідь",
         "de": "Schreibe die Antwort",
         "pl": "Napisz odpowiedź",
         "en": "Write the answer"},
 
     "sendbtn2": 
         {"cz": "Odeslat", 
-        "ua": "Надіслати",
+        "uk": "Надіслати",
         "de": "Senden",
         "pl": "Wysłać",
         "en": "Send"},
 
     "hUpozorneni": 
         {"cz": "Upozornění od učitele", 
-        "ua": "Повідомлення вчителя",
+        "uk": "Повідомлення вчителя",
         "de": "Hinweis vom Lehrer",
         "pl": "Zawiadomienie od nauczyciela",
         "en": "Notice from the teacher"},
